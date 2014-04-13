bf9cfd353eb115b3707787b95b5ef9aa59da755d
pdawsonsa <pdawsonsa@gmail.com>
2014-04-13 11:11:57 +0200

    initial commit

---
+++ FloatCellsScript.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 12 15:12:17 2014
+
+@author: peterDawson
+
+CBT700 Project: Controlability analysis of flotation bank
+"""
+
+
+import control as cn
+import numpy as np
+import scipy.linalg as spla
+import sympy as sm
