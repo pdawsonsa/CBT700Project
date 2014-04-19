ea3ba0ddf68c8b19f4b631f1b10a7d1ded432c44
pdawsonsa <pdawsonsa@gmail.com>
2014-04-19 17:41:56 +0200

    Before the penny dropped and i started to make some progress

--- Project.py
+++ Project.py
@@ -18,9 +18,9 @@ import matplotlib.pyplot as plt
 
 
 
-Kp = np.array([[-300., -150., -50.], 
-               [250., -250., -120.], 
-               [125., 100., -75.]])  
+Kp = np.array([[-30., -15., -5.], 
+               [25., -25., -12.], 
+               [12.5, 10., -7.50]])  
 taup = np.array([[120., 150., 100.],
                 [80., 110., 200.],
                 [60., 20., 70.]])
@@ -70,51 +70,62 @@ def _poles():
 
 #Frequency response of elements in matrix Gp:  Bode plots
 
-#plt.figure(1)
-#plt.clf()
-#rows, cols = np.shape(Kp)
-#for i in range(rows):
-#    for j in range(cols):
-#        plt.subplot(rows,cols,(3*j) + (i+1))
-#        omega = np.linspace(0.001,10,10000)
-#        GpMag = np.zeros((len(omega)))
-#        GpMag = np.abs(_Gp(omega*1j,i,j))
-##        GpMag = 20*np.log(np.abs(_Gp(omega*1j,i,j)))
-#        plt.loglog(omega,GpMag,'r-')
-#        plt.axis([np.min(omega), np.max(omega), 0, 1000])
-#        plt.grid(True)
-#        plt.xlabel('Freq')
-#        plt.ylabel('Mag')
-#fig = plt.gcf()
-#fig.subplots_adjust(bottom=0.05) 
-#fig.subplots_adjust(top=0.95) 
-#fig.subplots_adjust(left=0.05) 
-#fig.subplots_adjust(right=0.99)  
-#plt.suptitle('Magnitude of frequency responce')      
-#plt.show()
+plt.figure(1)
+plt.clf()
+rows, cols = np.shape(Kp)
+Kc = 1
+for i in range(rows):
+    for j in range(cols):
+        plt.subplot(rows,cols,(3*j) + (i+1))
+        omega = np.linspace(0.001,10,10000)
+        GpMag = np.zeros((len(omega)))
+        def _Gp1(s):
+            G = Kp[i,j]*np.exp(-Dp[i,j]*s)/(taup[i,j]*s + 1)
+            return(G)
+        GpMagL = np.abs(Kc*_Gp1(omega*1j))
+        GpMagS = np.abs(1/(1 + (Kc*_Gp1(omega*1j))))
+        GpMagT = np.abs((Kc*_Gp1(omega*1j))/(1 + (Kc*_Gp1(omega*1j))))
+#        GpMag = 20*np.log(np.abs(_Gp(omega*1j,i,j)))
+        plt.loglog(omega,GpMagL,'r-')
+        plt.loglog(omega,GpMagS,'b-')
+        plt.loglog(omega,GpMagT,'g-')
+        plt.axis([np.min(omega), np.max(omega), 0, 100])
+        plt.grid(True)
+        plt.xlabel('Freq')
+        plt.ylabel('Mag')
+fig = plt.gcf()
+fig.subplots_adjust(bottom=0.05) 
+fig.subplots_adjust(top=0.95) 
+fig.subplots_adjust(left=0.05) 
+fig.subplots_adjust(right=0.99)  
+plt.suptitle('Magnitude of frequency responce')      
+plt.show()
 
-#plt.figure(2)
-#plt.clf()
-#rows, cols = np.shape(Kp)
-#for i in range(rows):
-#    for j in range(cols):
-#        plt.subplot(rows,cols,(3*j) + (i+1))
-#        omega = np.linspace(0.001,10,10000)
-#        GpPhase = np.zeros((len(omega)))
-#        GpPhase = np.arg(_Gp(omega*1j,i,j))
-##        GpMag = 20*np.log(np.abs(_Gp(omega*1j,i,j)))
-#        plt.loglog(omega,GpPhase,'-')
-#        plt.axis([np.min(omega), np.max(omega), None, None])
-#        plt.grid(True)
-#        plt.xlabel('Freq')
-#        plt.ylabel('Phase Shift')
-#fig = plt.gcf()
-#fig.subplots_adjust(bottom=0.05) 
-#fig.subplots_adjust(top=0.95) 
-#fig.subplots_adjust(left=0.05) 
-#fig.subplots_adjust(right=0.99)  
-#plt.suptitle('Magnitude of frequency responce')      
-#plt.show()
+plt.figure(2)
+plt.clf()
+rows, cols = np.shape(Kp)
+for i in range(rows):
+    for j in range(cols):
+        plt.subplot(rows,cols,(3*j) + (i+1))
+        omega = np.linspace(0.001,10,10000)
+        GpPhase = np.zeros((len(omega)))
+        def _Gp1(s):
+            G = Kp[i,j]*np.exp(-Dp[i,j]*s)/(taup[i,j]*s + 1)
+            return(G)
+        GpPhase = np.angle(_Gp1(omega*1j), deg=True)
+#        GpMag = 20*np.log(np.abs(_Gp(omega*1j,i,j)))
+        plt.semilogx(omega,GpPhase,'r-')
+        plt.axis([np.min(omega), np.max(omega), -200, 200])
+        plt.grid(True)
+        plt.xlabel('Freq')
+        plt.ylabel('Phase shift')
+fig = plt.gcf()
+fig.subplots_adjust(bottom=0.05) 
+fig.subplots_adjust(top=0.95) 
+fig.subplots_adjust(left=0.05) 
+fig.subplots_adjust(right=0.99)  
+plt.suptitle('Magnitude of frequency responce')      
+plt.show()
 
     
 [U, S, T] = np.linalg.svd(_Gp(0.1))
