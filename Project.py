38de9c28f43a205152076453f0f0a7e5cde8dbfc
pdawsonsa <pdawsonsa@gmail.com>
2014-04-21 10:58:53 +0200

    added data from step tests

--- Project.py
+++ Project.py
@@ -18,15 +18,15 @@ import matplotlib.pyplot as plt
 
 
 
-Kp = np.array([[-30., -15., -5.], 
-               [25., -25., -12.], 
-               [12.5, 10., -7.50]])  
-taup = np.array([[120., 150., 100.],
-                [80., 110., 200.],
-                [60., 20., 70.]])
-Dp = np.array([[20., 40., 30.],
-               [25., 10., 18.],
-               [35., 15., 20.]])
+Kp = np.array([[35., 28., 10.], 
+               [-15., 35., 15.], 
+               [-10, -15., 20]])  
+taup = np.array([[50., 68., 60.],
+                [60., 47., 50.],
+                [40., 40., 45.]])
+Dp = np.array([[10., 15., 20.],
+               [15., 10., 15.],
+               [15., 15., 10.]])#*0.0  #multiply by 0.0 to remove effects of dead time.
 Kd = np.array([10., 6., 2.])
 taud = np.array([50., 70., 100.])
 Dd = np.array([5., 5., 5.])
@@ -53,6 +53,16 @@ def _SVD(s):
     S = 1/(np.eye(3) + L)
     T = L/(np.eye(3) + L)
     [U, S, V] = np.linalg.svd(L)
+    return(U, S, V)
+    
+    
+def _SVD_S(s):
+    K = 1
+    G = _Gp(s)
+    L = K*G
+    S = 1/(np.eye(3) + L)
+    T = L/(np.eye(3) + L)
+    [U, S, V] = np.linalg.svd(S)
     return(U, S, V)
     
     
@@ -100,8 +110,34 @@ def _bode():
     plt.ylabel('Condition number')
     plt.grid(True) 
     
-#    plt.figure(12)
-#    plt.clf()
+    plt.figure(12)
+    plt.clf()
+    plt.subplot(211)
+    plt.semilogx(omega, magPlot1dB, 'r-')
+    plt.semilogx(omega, magPlot2dB, 'b-')
+    plt.semilogx(omega, magPlot3dB, 'k-')
+    plt.semilogx(omega, np.ones((1000)), 'g-')
+    plt.xlabel('Frequency [rad/s]')
+    plt.ylabel('Singular value [dB]')
+    plt.grid(True)
+    plt.subplot(212)
+    plt.semilogx(omega, condNum, 'r-')
+    plt.xlabel('Frequency [rad/s]')
+    plt.ylabel('Condition number')
+    plt.grid(True)
+    magPlot1dB = np.zeros((len(omega)))
+    magPlot2dB = np.zeros((len(omega)))
+    magPlot3dB = np.zeros((len(omega)))
+    condNum = np.zeros((len(omega)))
+    for i in range(len(omega)):
+        U, S, V = _SVD_S(omega[i]*1j)
+        magPlot1[i] = (S[0])
+        magPlot2[i] = (S[1])
+        magPlot3[i] = (S[2])
+        magPlot1dB[i] = 20*np.log(S[0])
+        magPlot2dB[i] = 20*np.log(S[1])
+        magPlot3dB[i] = 20*np.log(S[2])
+        condNum[i] = S[0]/S[2] 
     plt.subplot(211)
     plt.semilogx(omega, magPlot1dB, 'r:')
     plt.semilogx(omega, magPlot2dB, 'b:')
@@ -111,7 +147,7 @@ def _bode():
     plt.ylabel('Singular value [dB]')
     plt.grid(True)
     plt.subplot(212)
-    plt.semilogx(omega, condNum, 'r-')
+    plt.semilogx(omega, condNum, 'b:')
     plt.xlabel('Frequency [rad/s]')
     plt.ylabel('Condition number')
     plt.grid(True)
