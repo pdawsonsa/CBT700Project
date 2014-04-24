718f4164aa34424c358e73d5e28ff96e5e08dfc7
pdawsonsa <pdawsonsa@gmail.com>
2014-04-24 16:40:51 +0200

    Added Bode plots with L, S and T, bandwidths and corssover points

--- Project.py
+++ Project.py
@@ -71,8 +71,6 @@ def poles():
     poles = np.zeros((dim))
     poles = -1/taup
     poleValues = np.zeros((9))
-#    poleDirs = np.zeros((9))
-#    poleValues = np.empty(9,dtype=complex)
     poleDirs = np.empty((3,9),dtype=complex)
     m = 0
     for i in poles:
@@ -91,58 +89,78 @@ for i in range(9):
     print('Output1 direction => %s'%(poleDirs[0,i]))
     print('Output2 direction => %s'%(poleDirs[1,i]))
     print('Output3 direction => %s'%(poleDirs[2,i]))
-#    print(S)
-#    print(V)
     print('')
 
+
 def bode():
+    omega = np.logspace(-3,2,1000)
+    magPlotL = np.zeros((len(omega)))
+    magPlotS = np.zeros((len(omega)))
+    magPlotT = np.zeros((len(omega)))
+    wB = np.zeros((3,3))
+    plt.figure(11)
+    plt.clf()    
+    for i in range(3):
+        for j in range(3):            
+            f = 0
+            for k in range(len(omega)):
+                magPlotL[k] = 20*np.log(np.abs(Gp(omega[k]*1j)[i,j]))
+                magPlotS[k] = 20*np.log(np.abs(1/(1 + Gp(omega[k]*1j)[i,j])))
+                magPlotT[k] = 20*np.log(np.abs(Gp(omega[k]*1j)[i,j]/(1 + Gp(omega[k]*1j)[i,j])))
+                if (f < 1 and magPlotS[k] > -3):
+                    wB[i,j] = omega[k]
+                    f = 1     
+            lineX = np.ones(1000)*wB[i,j]
+            lineY = np.linspace(-100, 100, 1000)
+            plt.subplot(3,3,3*i + j+1)
+            plt.semilogx(omega, magPlotL, 'r-', label='L')
+            plt.semilogx(omega, magPlotS, 'b-', label='S')
+            plt.semilogx(omega, magPlotT, 'k-', label='T')
+            plt.semilogx(omega, np.ones((1000))*-3, 'g-')
+            plt.semilogx(lineX, lineY, 'g-')
+#            plt.xlabel('Frequency [rad/s]')
+            plt.ylabel('Magnitude [dB]')
+            plt.title('Input%s => Output%s'%(j+1,i+1))
+            plt.legend()
+            plt.axis([None,None,-100,100])
+            plt.grid(True)
+    fig = plt.gcf()
+    fig.subplots_adjust(bottom=0.06) 
+    fig.subplots_adjust(top=0.95) 
+    fig.subplots_adjust(left=0.04) 
+    fig.subplots_adjust(right=0.99)
+    return(wB)
+    
+
+def bodeSVD():
     omega = np.logspace(-3,2,1000)
     magPlot1 = np.zeros((len(omega)))
     magPlot2 = np.zeros((len(omega)))
     magPlot3 = np.zeros((len(omega)))
-    magPlot1dB = np.zeros((len(omega)))
-    magPlot2dB = np.zeros((len(omega)))
-    magPlot3dB = np.zeros((len(omega)))
     condNum = np.zeros((len(omega)))
     f = 0
     for i in range(len(omega)):
         U, S, V = SVD(omega[i]*1j)
-        magPlot1[i] = (S[0])
-        magPlot2[i] = (S[1])
-        magPlot3[i] = (S[2])
-        magPlot1dB[i] = 20*np.log(S[0])
-        magPlot2dB[i] = 20*np.log(S[1])
-        magPlot3dB[i] = 20*np.log(S[2])
+        magPlot1[i] = 20*np.log(S[0])
+        magPlot2[i] = 20*np.log(S[1])
+        magPlot3[i] = 20*np.log(S[2])
         condNum[i] = S[0]/S[2]  
-        if (f < 1 and magPlot3[i] < 1):
+        if (f < 1 and magPlot3[i] < 0):
             crossOver = omega[i]
             f = 1
-     
-#    plt.figure(11)
-#    plt.clf()
-#    plt.subplot(211)
-#    plt.loglog(omega, magPlot1, 'r-')
-#    plt.loglog(omega, magPlot2, 'b-')
-#    plt.loglog(omega, magPlot3, 'k-')
-#    plt.loglog(omega, np.ones((1000)), 'g-')
-#    plt.xlabel('Frequency [rad/s]')
-#    plt.ylabel('Singular value')
-#    plt.grid(True)
-#    plt.subplot(212)
-#    plt.semilogx(omega, condNum, 'r-')
-#    plt.xlabel('Frequency [rad/s]')
-#    plt.ylabel('Condition number')
-#    plt.grid(True) 
-    
+    lineX = np.ones(1000)*crossOver
+    lineY = np.linspace(-100, 100, 1000)
     plt.figure(12)
     plt.clf()
     plt.subplot(211)
-    plt.semilogx(omega, magPlot1dB, 'r-')
-    plt.semilogx(omega, magPlot2dB, 'b-')
-    plt.semilogx(omega, magPlot3dB, 'k-')
+    plt.semilogx(omega, magPlot1, 'r-')
+    plt.semilogx(omega, magPlot2, 'b-')
+    plt.semilogx(omega, magPlot3, 'k-')
     plt.semilogx(omega, np.zeros((1000)), 'g-')
+    plt.semilogx(lineX, lineY, 'g-')
     plt.xlabel('Frequency [rad/s]')
     plt.ylabel('Singular value [dB]')
+    plt.axis([None,None,-100,100])
     plt.grid(True)
     plt.subplot(212)
     plt.semilogx(omega, condNum, 'r-')
@@ -150,11 +168,10 @@ def bode():
     plt.ylabel('Condition number')
     plt.grid(True)
     return(crossOver)
-    
+
+
 
     
-#[U, S, V] = np.linalg.svd(Gp(0.1))
-#[U, S, V] = np.linalg.svd([[5, 4],[3, 2]])  #Example 3.3 in Skogestad give me the correct SVD elements
 
 
 print('Gp matrix:')
@@ -163,45 +180,18 @@ print('')
 print('Gd matrix:')
 print(Gd(0.1))
 print('')
-#print('Poles of Gp:')
-#print(poles())
-#print('')
-##print('Zeros of Gp:')
-##print(_zeros())
-##print('')
-#
-#U, S, V = SVD(0.1)
-#print('U values')
-#print(U)
-#print('')
-#print('S values')
-#print(S)
-#print('')
-#print('T values')
-#print(V)
-#print('')
 
 print('')
-print('The crossover frequency is: %s rad/s'%(np.round(bode(),3)))
+print('The bandwidth frequency in rad/s is:')
+print((np.round(bode(),3)))
 print('')
 
 
-#def minsigma(s):
-#    U, Sigma, V = np.linalg.svd(Gp(s))
-#    return Sigma.min()
-#
-#plt.
-#complex_plot(minsigma, (-5, 5), (-5, 5))
+print('')
+print('The crossover frequency is: %s rad/s'%(np.round(bodeSVD(),3)))
+print('')
 
-#zeros1 = np.zeros((100),dtype=complex)
-#a = linspace(-5,1,100)
-#m = 0
-#for i in range(len(a)):
-##    for k in range(len(a)):
-#        U,S,V = la.svd(Gp(a[i]*1j))
-#        zeros1[m] = S.min()
-#        m += 1
-#
-#print(zeros1)
+
+
 
 print('============================== END ==================================')        
