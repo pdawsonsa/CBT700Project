5d03d467864aad09f7fcd10ed6d6e32899a70fc0
unknown <pedawson@KANWKS0941.fqm.internal>
2014-04-24 10:31:07 +0200

    Made changes to function naming removing the underscores and added bode plots with dB on y-axis

--- Project.py
+++ Project.py
@@ -32,23 +32,23 @@ taud = np.array([50., 70., 100.])
 Dd = np.array([5., 5., 5.])
 
 
-def _Gp(s):
+def Gp(s):
     dim = np.shape(Kp)
     G = np.zeros((dim))
     G = Kp*np.exp(-Dp*s)/(taup*s + 1)
     return(G)
     
 
-def _Gd(s):
+def Gd(s):
     dim = np.shape(Kd)
     G = np.zeros((dim))
     G = Kd*np.exp(-Dd*s)/(taud*s + 1)
     return(G)
     
     
-def _SVD(s):   #SVD of L = KGp
+def SVD(s):   #SVD of L = KGp
     K = 1.
-    G = _Gp(s)
+    G = Gp(s)
     L = K*G
     S = 1/(np.eye(3) + L)
     T = L/(np.eye(3) + L)
@@ -56,9 +56,9 @@ def _SVD(s):   #SVD of L = KGp
     return(U, S, V)
     
     
-def _SVD_S(s):   #SVD of S = 1/(I + L)
+def SVD_S(s):   #SVD of S = 1/(I + L)
     K = 1.
-    G = _Gp(s)
+    G = Gp(s)
     L = K*G
     S = 1/(np.eye(3) + L)
     T = L/(np.eye(3) + L)
@@ -66,7 +66,7 @@ def _SVD_S(s):   #SVD of S = 1/(I + L)
     return(U, S, V)
     
     
-def _poles():  
+def poles():  
     dim = np.shape(Kp)    
     poles = np.zeros((dim))
     poles = -1/taup
@@ -81,11 +81,11 @@ def _poles():
             m = m + 1
     m = 0
     for pole in poleValues:
-        U, S, V = _SVD(pole*1j)
+        U, S, V = SVD(pole*1j)
         poleDirs[:,m] = U[:,0]
         m = m + 1
     return(poleValues, poleDirs, U) 
-poleValues, poleDirs, U = _poles()
+poleValues, poleDirs, U = poles()
 for i in range(9):
     print('Pole => %s'%(round(poleValues[i], 4)))
     print('Output1 direction => %s'%(poleDirs[0,i]))
@@ -95,7 +95,7 @@ for i in range(9):
 #    print(V)
     print('')
 
-def _bode():
+def bode():
     omega = np.logspace(-3,2,1000)
     magPlot1 = np.zeros((len(omega)))
     magPlot2 = np.zeros((len(omega)))
@@ -104,23 +104,43 @@ def _bode():
     magPlot2dB = np.zeros((len(omega)))
     magPlot3dB = np.zeros((len(omega)))
     condNum = np.zeros((len(omega)))
+    f = 0
     for i in range(len(omega)):
-        U, S, V = _SVD(omega[i]*1j)
+        U, S, V = SVD(omega[i]*1j)
         magPlot1[i] = (S[0])
         magPlot2[i] = (S[1])
         magPlot3[i] = (S[2])
         magPlot1dB[i] = 20*np.log(S[0])
         magPlot2dB[i] = 20*np.log(S[1])
         magPlot3dB[i] = 20*np.log(S[2])
-        condNum[i] = S[0]/S[2]        
+        condNum[i] = S[0]/S[2]  
+        if (f < 1 and magPlot3[i] < 1):
+            crossOver = omega[i]
+            f = 1
      
-    plt.figure(11)
+#    plt.figure(11)
+#    plt.clf()
+#    plt.subplot(211)
+#    plt.loglog(omega, magPlot1, 'r-')
+#    plt.loglog(omega, magPlot2, 'b-')
+#    plt.loglog(omega, magPlot3, 'k-')
+#    plt.loglog(omega, np.ones((1000)), 'g-')
+#    plt.xlabel('Frequency [rad/s]')
+#    plt.ylabel('Singular value')
+#    plt.grid(True)
+#    plt.subplot(212)
+#    plt.semilogx(omega, condNum, 'r-')
+#    plt.xlabel('Frequency [rad/s]')
+#    plt.ylabel('Condition number')
+#    plt.grid(True) 
+    
+    plt.figure(12)
     plt.clf()
     plt.subplot(211)
-    plt.loglog(omega, magPlot1, 'r-')
-    plt.loglog(omega, magPlot2, 'b-')
-    plt.loglog(omega, magPlot3, 'k-')
-    plt.loglog(omega, np.ones((1000)), 'g-')
+    plt.semilogx(omega, magPlot1dB, 'r-')
+    plt.semilogx(omega, magPlot2dB, 'b-')
+    plt.semilogx(omega, magPlot3dB, 'k-')
+    plt.semilogx(omega, np.zeros((1000)), 'g-')
     plt.xlabel('Frequency [rad/s]')
     plt.ylabel('Singular value [dB]')
     plt.grid(True)
@@ -128,28 +148,29 @@ def _bode():
     plt.semilogx(omega, condNum, 'r-')
     plt.xlabel('Frequency [rad/s]')
     plt.ylabel('Condition number')
-    plt.grid(True) 
+    plt.grid(True)
+    return(crossOver)
     
 
     
-#[U, S, V] = np.linalg.svd(_Gp(0.1))
+#[U, S, V] = np.linalg.svd(Gp(0.1))
 #[U, S, V] = np.linalg.svd([[5, 4],[3, 2]])  #Example 3.3 in Skogestad give me the correct SVD elements
 
 
-#print('Gp matrix:')
-#print(_Gp(0.1))
-#print('')
-#print('Gd matrix:')
-#print(_Gd(0.1))
-#print('')
+print('Gp matrix:')
+print(Gp(0.1))
+print('')
+print('Gd matrix:')
+print(Gd(0.1))
+print('')
 #print('Poles of Gp:')
-#print(_poles())
+#print(poles())
 #print('')
 ##print('Zeros of Gp:')
 ##print(_zeros())
 ##print('')
 #
-#U, S, V = _SVD(0.1)
+#U, S, V = SVD(0.1)
 #print('U values')
 #print(U)
 #print('')
@@ -160,12 +181,13 @@ def _bode():
 #print(V)
 #print('')
 
-_bode()
-
+print('')
+print('The crossover frequency is: %s rad/s'%(np.round(bode(),3)))
+print('')
 
 
 #def minsigma(s):
-#    U, Sigma, V = np.linalg.svd(_Gp(s))
+#    U, Sigma, V = np.linalg.svd(Gp(s))
 #    return Sigma.min()
 #
 #plt.
@@ -176,12 +198,10 @@ _bode()
 #m = 0
 #for i in range(len(a)):
 ##    for k in range(len(a)):
-#        U,S,V = la.svd(_Gp(a[i]*1j))
+#        U,S,V = la.svd(Gp(a[i]*1j))
 #        zeros1[m] = S.min()
 #        m += 1
 #
 #print(zeros1)
 
-
-
-        
+print('============================== END ==================================')        
