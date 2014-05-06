Fixed the scaling of the disturbance matrix

Project.py
@@ -32,9 +32,9 @@ taup = np.array([[52., 68., 60.],
 Dp = np.array([[10., 15., 20.],
                [15., 10., 15.],
                [15., 15., 10.]])*1  #multiply by 0.0 to remove effects of dead time.
-Kd = np.array([1., 1., 0.8])
+Kd = np.array([-30., -30., -24])
 taud = np.array([40., 35., 30.])
-Dd = np.array([60., 65., 70.])
+Dd = np.array([30., 40., 50.])
 Kc = np.array([[1., 0., 0.], 
                [0., 1., 0.], 
                [0., 0., 1.]])
@@ -135,72 +135,7 @@ def bode():
     fig.subplots_adjust(left=0.04) 
     fig.subplots_adjust(right=0.99)
     
-    
 
-#def bodeSVD():
-#    omega = np.logspace(-3,2,1000)
-#    magPlotL1 = np.zeros((len(omega)))
-#    magPlotL2 = np.zeros((len(omega)))
-#    magPlotL3 = np.zeros((len(omega)))
-#    magPlotS1 = np.zeros((len(omega)))
-#    magPlotS2 = np.zeros((len(omega)))
-#    magPlotS3 = np.zeros((len(omega)))
-#    condNum = np.zeros((len(omega)))
-#    f = 0
-#    ff = 0                                                    #f for flag
-#    for i in range(len(omega)):
-#        U_L, S_L, V_L, U_S, S_S, V_S = SVD(omega[i]*1j)
-#        magPlotL1[i] = 20*np.log(S_L[0])
-#        magPlotL2[i] = 20*np.log(S_L[1])
-#        magPlotL3[i] = 20*np.log(S_L[2])
-#        magPlotS1[i] = 20*np.log(S_S[0])
-#        magPlotS2[i] = 20*np.log(S_S[1])
-#        magPlotS3[i] = 20*np.log(S_S[2])
-#        condNum[i] = S_L[0]/S_L[2]  
-#        if (f < 1 and magPlotL3[i] < 0):
-#            wC = omega[i]
-#            f = 1
-#        if (ff < 1 and magPlotS1[i] > -3):
-#            wB = omega[i]
-#            ff = 1                                                     
-#    lineX = np.ones(1000)*wB
-#    lineY = np.linspace(-100, 100, 1000)
-#    lineX1 = np.ones(1000)*wC
-#    lineY1 = np.linspace(-100, 100, 1000)
-#    plt.figure(12)
-#    plt.clf()
-#    plt.subplot(211)
-#    plt.semilogx(omega, magPlotL1, 'r-', label = 'G Max $\sigma$')
-#    plt.semilogx(omega, magPlotL3, 'k-', label = 'G Min $\sigma$')
-#    plt.semilogx(omega, magPlotS1, 'r:', label = 'S Max $\sigma$', lw=2)
-#    plt.semilogx(omega, magPlotS3, 'k:', label = 'S Min $\sigma$', lw=2)
-#    plt.semilogx(omega, np.ones((1000))*-3, 'g-')
-#    plt.semilogx(omega, np.ones((1000))*0, 'b-')
-#    plt.semilogx(lineX, lineY, 'g-')
-#    plt.semilogx(lineX1, lineY1, 'b-')
-#    plt.text(0.0015,15,'wB = %s rad/s'%(np.round(wB,3)), color='green')
-#    plt.text(0.0015,-20,'wC = %s rad/s'%(np.round(wC,3)), color='blue')
-#    plt.xlabel('Frequency [rad/s]')
-#    plt.ylabel('Singular value [dB]')
-#    plt.axis([None,None,-100,100])
-#    plt.legend(fontsize=12)
-#    fig = plt.gcf()
-#    BG = fig.patch
-#    BG.set_facecolor('white')
-#    plt.grid(True)
-#    plt.subplot(212)
-#    lineX = np.ones(1000)*wB
-#    lineY = np.linspace(0, 10, 1000)
-#    plt.semilogx(omega, condNum, 'r-')
-#    plt.semilogx(lineX, lineY, 'g-')
-#    plt.text(0.002,0.5,'wB = %s rad/s'%(np.round(wB,3)), color='green')
-#    plt.xlabel('Frequency [rad/s]')
-#    plt.ylabel('Condition number')
-#    plt.grid(True)
-#    fig = plt.gcf()
-#    BG = fig.patch
-#    BG.set_facecolor('white')
-#    return(wB,wC)
 
 
 def bodeSVD():