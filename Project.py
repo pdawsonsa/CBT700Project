further fixes to scaling, added disturbance rejection and started uncertainty analysis

Project.py
@@ -23,21 +23,21 @@ from utils import RGA
 #======================= ASSIGNING OF GLOBAL VARIABLES =======================
 
 
-Kp = np.array([[28., 22.4, 8.], 
-               [-12., 28., 12.], 
-               [-8, -12., 16]])  
+Kp = np.array([[63., 39.2, 8.], 
+               [-27., 49., 12.], 
+               [-18, -21., 16]])  
 taup = np.array([[52., 68., 60.],
                 [61., 47., 50.],
                 [40., 41., 45.]])
 Dp = np.array([[10., 15., 20.],
                [15., 10., 15.],
                [15., 15., 10.]])*1  #multiply by 0.0 to remove effects of dead time.
-Kd = np.array([-30., -30., -24])
+Kd = np.array([-12., -12., -9.6])
 taud = np.array([40., 35., 30.])
 Dd = np.array([30., 40., 50.])
 Kc = np.array([[1., 0., 0.], 
                [0., 1., 0.], 
-               [0., 0., 1.]])
+               [0., 0., 1.]])*1
 #Kc = 1
 
 #=============================================================================
@@ -63,11 +63,12 @@ def Gd(s):
 def SVD(s):   #SVD of L = KGp
     G = Gp(s)
     L = Kc*G
-    S1 = la.inv((np.eye(3) + L))
-#    T = L/S1
+    S = la.inv((np.eye(3) + L))
+    T = L*S
     [U_L, S_L, V_L] = np.linalg.svd(G)
-    [U_S, S_S, V_S] = np.linalg.svd(S1)
-    return(U_L, S_L, V_L, U_S, S_S, V_S)
+    [U_S, S_S, V_S] = np.linalg.svd(S)
+    [U_T, S_T, V_T] = np.linalg.svd(T)
+    return(U_L, S_L, V_L, U_S, S_S, V_S, U_T, S_T, V_T)
     
   
     
@@ -99,7 +100,7 @@ def bode():
     magPlotS = np.zeros((len(omega)))
     magPlotT = np.zeros((len(omega)))
     wB = np.zeros((3,3))
-    plt.figure(11)
+    plt.figure(1)
     plt.clf()    
     for i in range(3):
         for j in range(3):            
@@ -137,7 +138,6 @@ def bode():
     
 
 
-
 def bodeSVD():
     omega = np.logspace(-3,2,1000)
     magPlotL1 = np.zeros((len(omega)))
@@ -146,17 +146,23 @@ def bodeSVD():
     magPlotS1 = np.zeros((len(omega)))
     magPlotS2 = np.zeros((len(omega)))
     magPlotS3 = np.zeros((len(omega)))
+    magPlotT1 = np.zeros((len(omega)))
+    magPlotT2 = np.zeros((len(omega)))
+    magPlotT3 = np.zeros((len(omega)))
     condNum = np.zeros((len(omega)))
     f = 0
     ff = 0                                                    #f for flag
     for i in range(len(omega)):
-        U_L, S_L, V_L, U_S, S_S, V_S = SVD(omega[i]*1j)
+        U_L, S_L, V_L, U_S, S_S, V_S, U_T, S_T, V_T = SVD(omega[i]*1j)   # Not optimal, revise!
         magPlotL1[i] = S_L[0]
         magPlotL2[i] = S_L[1]
         magPlotL3[i] = S_L[2]
         magPlotS1[i] = S_S[0]
         magPlotS2[i] = S_S[1]
         magPlotS3[i] = S_S[2]
+        magPlotT1[i] = S_T[0]
+        magPlotT2[i] = S_T[1]
+        magPlotT3[i] = S_T[2]
         condNum[i] = S_L[0]/S_L[2]  
         if (f < 1 and magPlotL3[i] < 1):
             wC = omega[i]
@@ -168,13 +174,15 @@ def bodeSVD():
     lineY = np.linspace(0.001, 100, 1000)
     lineX1 = np.ones(1000)*wC
     lineY1 = np.linspace(0.001, 100, 1000)
-    plt.figure(12)
+    plt.figure(2)
     plt.clf()
     plt.subplot(211)
     plt.loglog(omega, magPlotL1, 'r-', label = 'G Max $\sigma$')
-    plt.loglog(omega, magPlotL3, 'k-', label = 'G Min $\sigma$')
-    plt.loglog(omega, magPlotS1, 'r:', label = 'S Max $\sigma$', lw=2)
+    plt.loglog(omega, magPlotL3, 'r:', label = 'G Min $\sigma$', lw=2)
+    plt.loglog(omega, magPlotS1, 'k-', label = 'S Max $\sigma$')
     plt.loglog(omega, magPlotS3, 'k:', label = 'S Min $\sigma$', lw=2)
+    plt.loglog(omega, magPlotT1, 'b-', label = 'T Max $\sigma$')
+    plt.loglog(omega, magPlotT3, 'b:', label = 'T Min $\sigma$', lw=2)    
     plt.loglog(omega, np.ones((1000))*0.707, 'g-')
     plt.loglog(omega, np.ones((1000))*1, 'b-')
     plt.loglog(lineX, lineY, 'g-')
@@ -183,7 +191,7 @@ def bodeSVD():
     plt.text(0.0015,1.5,'wC = %s rad/s'%(np.round(wC,3)), color='blue')
     plt.xlabel('Frequency [rad/s]')
     plt.ylabel('Singular value [dB]')
-    plt.axis([None,None,0.01,100])
+    plt.axis([None,None,0.001,100])
     plt.legend(fontsize=12)
     fig = plt.gcf()
     BG = fig.patch
@@ -201,7 +209,13 @@ def bodeSVD():
     fig = plt.gcf()
     BG = fig.patch
     BG.set_facecolor('white')
+    fig.subplots_adjust(bottom=0.2) 
+    fig.subplots_adjust(top=0.9) 
+    fig.subplots_adjust(left=0.2) 
+    fig.subplots_adjust(right=0.9)
+    plt.grid(True)
     return(wB,wC)
+    
     
     
 def RGAw():
@@ -224,7 +238,7 @@ def RGAw():
         RGAvalues[i,7] = (RGAm[1,2])
         RGAvalues[i,8] = (RGAm[2,2])
         RGAnum[i] = np.sum(RGAm - np.identity(3))
-    plt.figure(21)
+    plt.figure(3)
     plt.clf()
     for i in range(3):
         for j in range(3):
@@ -249,8 +263,9 @@ def RGAw():
     fig.subplots_adjust(top=0.91) 
     fig.subplots_adjust(left=0.08) 
     fig.subplots_adjust(right=0.98)
-    plt.figure(22)
+    plt.figure(4)
     plt.clf()
+    plt.subplot(212)
     plt.semilogx(omega, RGAnum, 'b-')
     BG = fig.patch
     BG.set_facecolor('white')
@@ -260,11 +275,118 @@ def RGAw():
     plt.text(0.002,3.6,'min wB = %s rad/s'%(np.round(np.min(wB),3)), color='green', fontsize=10)
     plt.title('RGA number at varying frequencies', size=16)
     plt.ylabel('RGA number')
+    plt.xlabel('Frequency [rad/s)]')
+    fig = plt.gcf()
+    BG = fig.patch
+    BG.set_facecolor('white')
+    fig.subplots_adjust(bottom=0.2) 
+    fig.subplots_adjust(top=0.9) 
+    fig.subplots_adjust(left=0.2) 
+    fig.subplots_adjust(right=0.9)
     plt.grid(True)
     
     
 
+def distRej():
+    omega = np.logspace(-3,1,1000)
+    L1 = np.zeros((len(omega)))
+    L2 = np.zeros((len(omega)))
+    Gd1 = np.zeros((len(omega)))
+    for i in range(len(omega)):
+        U_L, S_L, V_L, U_S, S_S, V_S, U_T, S_T, V_T = SVD(omega[i]*1j)
+        L1[i] = S_S[2]                      #S = 1/|L + 1| 
+        L2[i] = S_S[0]
+        Gd1[i] = 1/la.norm(Gd(omega[i]*1j),2)   #Returns largest sing value of Gd(wj)
+    plt.figure(5)
+    plt.clf()
+    plt.subplot(211)
+    plt.loglog(omega, L1, 'r-', label = 'min $\sigma$S')
+    plt.loglog(omega, L2, 'r:', lw=2, label = 'max $\sigma$S')
+    plt.loglog(omega, Gd1, 'k-', label = '1/||Gd||$_2$')
+    plt.ylabel('Magnitude')
+    plt.xlabel('Frequency [rad/s)]')
+    plt.axis([None, None, None, None])
+    plt.grid(True)
+    plt.legend(fontsize = 12)
+    fig = plt.gcf()
+    BG = fig.patch
+    BG.set_facecolor('white')
+    fig.subplots_adjust(bottom=0.2) 
+    fig.subplots_adjust(top=0.9) 
+    fig.subplots_adjust(left=0.2) 
+    fig.subplots_adjust(right=0.9)
+    
 
+
+def perfectControl():
+    #For perfect cotnrol
+    omega = np.logspace(-3,1,1000)
+    Gd1 = np.zeros((len(omega)))
+    Gd2 = np.zeros((len(omega)))
+    Gd3 = np.zeros((len(omega)))
+    for i in range(len(omega)):
+        G = Gp(omega[i]*1j)
+        Gdt = Gd(omega[i]*1j)               #Gdt just a temp assignmnet for Gd
+        Gd1[i] = la.norm(la.inv(G)*Gdt[0], ord=inf)
+        Gd2[i] = la.norm(la.inv(G)*Gdt[1], ord=inf)
+        Gd3[i] = la.norm(la.inv(G)*Gdt[2], ord=inf)
+    plt.figure(6)
+    plt.clf()
+    plt.subplot(211)
+    plt.semilogx(omega, Gd1, 'r-', label = '||G$^{-1}$g$_d$1||$_{max}$')
+    plt.semilogx(omega, Gd2, 'k-', label = '||G$^{-1}$g$_d$2||$_{max}$')
+    plt.semilogx(omega, Gd3, 'b-', label = '||G$^{-1}$g$_d$3||$_{max}$')
+    plt.ylabel('Magnitude')
+    plt.xlabel('Frequency [rad/s)]')
+    plt.axis([None, None, None, None])
+    plt.grid(True)
+    plt.legend(fontsize = 12)
+    fig = plt.gcf()
+    BG = fig.patch
+    BG.set_facecolor('white')
+    fig.subplots_adjust(bottom=0.2) 
+    fig.subplots_adjust(top=0.9) 
+    fig.subplots_adjust(left=0.2) 
+    fig.subplots_adjust(right=0.9)
+    #For acceptable control
+    S1 = np.zeros((len(omega)))
+    S2 = np.zeros((len(omega)))
+    S3 = np.zeros((len(omega)))
+    Gd1 = np.zeros((len(omega)))
+    Gd2 = np.zeros((len(omega)))
+    Gd3 = np.zeros((len(omega)))
+    for i in range(len(omega)):
+        U,S,V = la.svd(Gp(omega[i]*1j))
+        Gdt = Gd(omega[i]*1j)               #Gdt just a temp assignmnet for Gd
+        S1[i] = S[0]
+        S2[i] = S[1]
+        S3[i] = S[2]
+        Gd1[i] = np.max(np.abs(np.transpose(np.conj(U[0]))*Gdt[0]) - 1)
+        Gd2[i] = np.max(np.abs(np.transpose(np.conj(U[1]))*Gdt[1]) - 1)
+        Gd3[i] = np.max(np.abs(np.transpose(np.conj(U[2]))*Gdt[2]) - 1)
+    plt.figure(6)
+    plt.subplot(212)
+    plt.semilogx(omega, S1, 'r-', label = '$\sigma$$_1$(G)')
+    plt.semilogx(omega, Gd1, 'r:', label = '|u$_1$$^H$g$_d$|')
+    plt.semilogx(omega, S2, 'k-', label = '$\sigma$$_2$(G)')
+    plt.semilogx(omega, Gd2, 'k:', label = '|u$_2$$^H$g$_d$|')
+    plt.semilogx(omega, S3, 'b-', label = '$\sigma$$_3$(G)')
+    plt.semilogx(omega, Gd3, 'b:', label = '|u$_3$$^H$g$_d$|')
+    plt.ylabel('Magnitude')
+    plt.xlabel('Frequency [rad/s)]')
+    plt.axis([None, None, None, None])
+    plt.grid(True)
+    plt.legend(fontsize = 12)
+    fig = plt.gcf()
+    BG = fig.patch
+    BG.set_facecolor('white')
+    fig.subplots_adjust(bottom=0.2) 
+    fig.subplots_adjust(top=0.9) 
+    fig.subplots_adjust(left=0.2) 
+    fig.subplots_adjust(right=0.9)        
+        
+    
+    
 
 
 #=============================================================================
@@ -289,7 +411,12 @@ def RGAw():
 bode()
 wB, wC = bodeSVD()
 RGAw()
-U_L, S_L, V_L, U_S, S_S, V_S = SVD(0)
+U_L, S_L, V_L, U_S, S_S, V_S, U_T, S_T, V_T = SVD(0)
+distRej()
+perfectControl()
+
+
+
 
 print('The steady state SVD of the system is:')
 print('')