bffbf8da85d6e57b693578fd82cad86a3526173e
pdawsonsa <pdawsonsa@gmail.com>
2014-05-22 13:44:30 +0200

    Fixed the performance criteria, removed random Bode plots and added PID controller

--- Project.py
+++ Project.py
@@ -21,15 +21,15 @@ import utils
 #======================= ASSIGNING OF GLOBAL VARIABLES =======================
 
 #Transfer function parameters
-Kp = np.array([[63., 39.2, 8.], 
-               [-27., 49., 12.], 
-               [-18., -21., 16.]])  
+Kp = np.array([[63., 39.2, 10.], 
+               [-27., 49., 16.], 
+               [-18., -21., 20.]])  
 taup = np.array([[52., 68., 60.],
                 [61., 47., 50.],
                 [40., 41., 45.]])
 Dp = np.array([[10., 15., 20.],
                [15., 10., 15.],
-               [15., 15., 10.]])*1    
+               [15., 15., 10.]])*1  
 Kd = np.array([[-2.25],[-1.75],[-0.8]])
 taud = np.array([[40.],[35.],[30.]])
 Dd = np.array([[30.],[40.],[50.]])
@@ -38,22 +38,30 @@ Dd = np.array([[30.],[40.],[50.]])
 #Controller
 #Kc = np.array([[0.09, 0., 0.], 
 #               [0., 0.112, 0.],            #Plant as found controller
-#               [0., 0., 0.064]])
-Kc = np.array([[0.1, 0., 0.], 
-               [0., 0.4, 0.], 
-               [0., 0., 0.4]])
-
-
+#               [0., 0., 0.15]])
+KcP = np.array([[0.09, 0., 0.], 
+                [0., 0.112, 0.],     
+                [0., 0., 0.15]])
+KcI = np.array([[400., 1., 1.], 
+                [1., 400., 1.],     
+                [1., 1., 400.]])
+KcD = np.array([[2., 0., 0.], 
+                [0., 2., 0.],     
+                [0., 0., 2.]])               
+               
+ 
 
 #=============================================================================
 #========================== DEFINING OF FUNCTIONS ============================
 
+def Kc(s):
+    return(KcP*(1 + 1/(KcI*s) + KcD*s))   # PI Controller
 
 def G(s):
     return(Kp*np.exp(-Dp*s)/(taup*s + 1))
     
 def L(s):
-    return(Kc*G(s))                         #SVD of L = KG)
+    return(Kc(s)*G(s))                         #SVD of L = KG)
     
 def S(s):
     return(la.inv((np.eye(3) + L(s))))      #SVD of S = 1/(I + L)
@@ -92,81 +100,6 @@ def poles():
 
 
 
-def bodeSVD():
-    w = np.logspace(-3,0,1000)
-    magPlotL1 = np.zeros((len(w)))
-    magPlotL3 = np.zeros((len(w)))
-    magPlotS1 = np.zeros((len(w)))
-    magPlotS3 = np.zeros((len(w)))
-    magPlotT1 = np.zeros((len(w)))
-    magPlotT3 = np.zeros((len(w)))
-    condNum = np.zeros((len(w)))
-    f = 0
-    ff = 0                                                    #f for flag
-    for i in range(len(w)):
-        U_G, Sv_G, V_G = utils.SVD(G(w[i]*1j))
-        U_L, Sv_L, V_L = utils.SVD(L(w[i]*1j))
-        U_S, Sv_S, V_S = utils.SVD(S(w[i]*1j))
-        U_T, Sv_T, V_T = utils.SVD(T(w[i]*1j))  
-        magPlotL1[i] = Sv_G[0]
-        magPlotL3[i] = Sv_G[2]
-        magPlotS1[i] = Sv_S[0]
-        magPlotS3[i] = Sv_S[2]
-        magPlotT1[i] = Sv_T[0]
-        magPlotT3[i] = Sv_T[2]
-        condNum[i] = Sv_G[0]/Sv_G[2]  
-        if (f < 1 and magPlotL3[i] < 1):
-            wC = w[i]
-            f = 1
-        if (ff < 1 and magPlotS1[i] > 0.707):
-            wB = w[i]
-            ff = 1                                                     
-    lineX = np.ones(10)*wB
-    lineY = np.linspace(0.001, 100, 10)
-    lineX1 = np.ones(10)*wC
-    lineY1 = np.linspace(0.001, 100, 10)
-    plt.figure(1)
-    plt.clf()
-    plt.subplot(211)
-    plt.loglog(w, magPlotL1, 'r-', label = 'G Max $\sigma$')
-    plt.loglog(w, magPlotL3, 'r-', label = 'G Min $\sigma$', alpha = 0.4)
-    plt.loglog(w, magPlotS1, 'k-', label = 'S Max $\sigma$')
-    plt.loglog(w, magPlotS3, 'k-', label = 'S Min $\sigma$', alpha = 0.4)
-    plt.loglog(w, np.ones((len(w)))*0.707, 'g-')
-    plt.loglog(w, np.ones((len(w)))*1, 'b-')
-    plt.loglog(lineX, lineY, 'g-')
-    plt.loglog(lineX1, lineY1, 'b-')
-    plt.text(0.0015,0.3,'wB = %s rad/s'%(np.round(wB,3)), color='green')
-    plt.text(0.0015,1.5,'wC = %s rad/s'%(np.round(wC,3)), color='blue')
-    plt.xlabel('Frequency [rad/s]')
-    plt.ylabel('Singular value [dB]')
-    plt.axis([None,None,0.01,100])
-    plt.legend(fontsize=12)
-    fig = plt.gcf()
-    BG = fig.patch
-    BG.set_facecolor('white')
-    plt.grid(True)
-    plt.subplot(212)
-    lineX = np.ones(10)*wB
-    lineY = np.linspace(0, 12, 10)
-    plt.semilogx(w, condNum, 'r-')
-    plt.semilogx(lineX, lineY, 'g-')
-    plt.text(0.0015,0.3,'wB = %s rad/s'%(np.round(wB,3)), color='green')
-    plt.xlabel('Frequency [rad/s]')
-    plt.ylabel('Condition number')
-    plt.grid(True)
-    fig = plt.gcf()
-    BG = fig.patch
-    BG.set_facecolor('white')
-    fig.subplots_adjust(bottom=0.2) 
-    fig.subplots_adjust(top=0.9) 
-    fig.subplots_adjust(left=0.2) 
-    fig.subplots_adjust(right=0.9)
-    plt.grid(True)
-    return(wB,wC)
-
-
-
 def perf_Wp():
     w = np.logspace(-3,0,1000)
     magPlotS1 = np.zeros((len(w)))
@@ -179,19 +112,23 @@ def perf_Wp():
         magPlotS3[i] = Sv[2]
         if (f < 1 and magPlotS1[i] > 0.707):
             wB = w[i]
-            f = 1  
+            f = 1
+    wB_ = 0.05      #20 sec 
     M = 2
-    A = utils.sigmas(S(0))[0]
+    A = 0.3
     for i in range(len(w)):
-        Wp[i] = np.abs((w[i]*1j/M + wB) / (w[i]*1j + wB*A))                                              
-    plt.figure(2)
+        Wp[i] = np.abs((w[i]*1j/M + wB_) / (w[i]*1j + wB_*A))                                              
+    plt.figure(1)
     plt.clf()
     plt.subplot(211)
     plt.loglog(w, magPlotS1, 'r-', label = 'Max $\sigma$(S)')
     plt.loglog(w, 1./Wp, 'k:', label = '|1/W$_P$|', lw=2)
+    plt.axvline(0.0333, color='blue', ls=':', lw='2')
     plt.axhline(0.707, color='green')
     plt.axvline(wB, color='green')
-    plt.text(0.0015,0.3,'wB = %s rad/s'%(np.round(wB,3)), color='green')
+    plt.axvline(0.0333, color='blue', ls=':', lw='2')
+    plt.text(0.015,7,'req wB', color='blue')
+    plt.text(wB*1.1, 0.12, 'wB = %s rad/s'%(np.round(wB,3)), color='green')
     plt.xlabel('Frequency [rad/s]')
     plt.ylabel('Magnitude')
     plt.axis([None,None,0.1,10])
@@ -200,7 +137,9 @@ def perf_Wp():
     plt.subplot(212)
     plt.semilogx(w, magPlotS1*Wp, 'r-')
     plt.axhline(1, color='blue', ls=':', lw=2)
-    plt.text(0.06, 1.85, '||W$_P$S||$_{inf}$')
+    plt.text(0.06, np.max(magPlotS1*Wp)*0.95, '||W$_P$S||$_{inf}$')
+    plt.axvline(0.0333, color='blue', ls=':', lw='2')
+    plt.text(0.015, np.max(magPlotS1*Wp)*0.95, 'req wB', color='blue')
     plt.xlabel('Frequency [rad/s]')
     plt.ylabel('Magnitude')
     fig = plt.gcf()
@@ -219,7 +158,7 @@ def RGAw():
     '''
     Computes the RGA for diagonal pairing at varying frequencies
     '''
-    w = np.logspace(-3,1,1000)
+    w = np.logspace(-3,0,1000)
     RGAvalues = np.zeros((len(w),9))
     RGAnum = np.zeros((len(w)))
     for i in range(len(w)):
@@ -235,7 +174,7 @@ def RGAw():
         RGAvalues[i,7] = (RGAm[1,2])
         RGAvalues[i,8] = (RGAm[2,2])
         RGAnum[i] = np.sum(RGAm - np.identity(3))
-    plt.figure(3)
+    plt.figure(2)
     plt.clf()
     for i in range(3):
         for j in range(3):
@@ -260,7 +199,7 @@ def RGAw():
     fig.subplots_adjust(top=0.91) 
     fig.subplots_adjust(left=0.08) 
     fig.subplots_adjust(right=0.98)
-    plt.figure(4)
+    plt.figure(3)
     plt.clf()
     plt.subplot(212)
     plt.semilogx(w, RGAnum, 'b-')
@@ -286,7 +225,7 @@ def RGAw():
     
 
 def distRej():
-    w = np.logspace(-3,1,1000)
+    w = np.logspace(-3,0,1000)
     S1 = np.zeros((len(w)))
     S2 = np.zeros((len(w)))
     Gd1 = np.zeros((len(w)))
@@ -299,7 +238,7 @@ def distRej():
         Gd1[i] = 1/la.norm(Gd(w[i]*1j),2)   #Returns largest sing value of Gd(wj)
         distCondNum[i] = la.norm(G(w[i]*1j),2)*la.norm(la.inv(G(w[i]*1j))*Gd1[i]*Gd(w[i]*1j),2)
         condNum[i] = la.norm(G(w[i]*1j),2)*la.norm(la.inv(G(w[i]*1j)),2)
-    plt.figure(5)
+    plt.figure(4)
     plt.clf()
     plt.subplot(211)
     lineX = np.ones(10)*wB
@@ -341,7 +280,7 @@ def distRej():
 
 def perfectControl():
     #For perfect cotnrol
-    w = np.logspace(-3,1,1000)
+    w = np.logspace(-3,0,1000)
     Gd1 = np.zeros((len(w)))
     Gd2 = np.zeros((len(w)))
     Gd3 = np.zeros((len(w)))
@@ -351,7 +290,7 @@ def perfectControl():
         Gd1[i] = la.norm(la.inv(Gt)*Gdt[0], ord=np.inf)
         Gd2[i] = la.norm(la.inv(Gt)*Gdt[1], ord=np.inf)
         Gd3[i] = la.norm(la.inv(Gt)*Gdt[2], ord=np.inf)
-    plt.figure(6)
+    plt.figure(5)
     plt.clf()
     plt.subplot(211)
     plt.semilogx(w, Gd1, 'r-', label = '||G$^{-1}$g$_d$1||$_{max}$')
@@ -406,18 +345,18 @@ def perfectControl():
     fig.subplots_adjust(right=0.9)   
     plt.show()     
 
+   
     
-    
-def nyqPlot():
+def MIMOnyqPlot():
     w = np.logspace(-3, 2, 10000)    
-    GL = np.zeros((len(w)), dtype=complex)
+    Lin = np.zeros((len(w)), dtype=complex)
     x = np.zeros((len(w)))
     y = np.zeros((len(w)))
     for i in range(len(w)):
-        GL[i] = la.det(np.eye(3) + L(w[i]*1j))
-        x[i] = np.real(GL[i])
-        y[i] = np.imag(GL[i])        
-    plt.figure(7)
+        Lin[i] = la.det(np.eye(3) + L(w[i]*1j))
+        x[i] = np.real(Lin[i])
+        y[i] = np.imag(Lin[i])        
+    plt.figure(6)
     plt.clf()
     plt.plot(x, y, 'k-', lw=1)
     plt.xlabel('Re G(wj)')
@@ -429,10 +368,18 @@ def nyqPlot():
     plt.plot(x, y_up, 'b:', x, y_down, 'b:', lw=2)
     plt.plot(0, 0, 'r*', ms = 10)
     plt.grid(True)
-    n = 10
+    n = 2
     plt.axis([-n,n,-n,n])
-    plt.show()
+    fig = plt.gcf()
+    BG = fig.patch
+    BG.set_facecolor('white')
+    fig.subplots_adjust(bottom=0.6) 
+    fig.subplots_adjust(top=0.9) 
+    fig.subplots_adjust(left=0.6) 
+    fig.subplots_adjust(right=0.9)   
+    plt.show()   
 
+ 
 
 
 #=============================================================================
@@ -446,7 +393,7 @@ def nyqPlot():
 #print(Gd(0))
 #print('')
 
-#poleValues, poleDirsIn, poleDirsOut = poles()
+poleValues, poleDirsIn, poleDirsOut = poles()
 #for i in range(9):
 #    print('Pole => %s'%(round(poleValues[i], 4)))
 #    print('Input1 direction => %s    Output1 direction => %s'%(poleDirsIn[0,i], poleDirsOut[0,i]))
@@ -454,12 +401,11 @@ def nyqPlot():
 #    print('Input3 direction => %s    Output3 direction => %s'%(poleDirsIn[2,i], poleDirsOut[2,i]))
 #    print('')
 
-wB, wC = bodeSVD()
-perf_Wp()
-#RGAw()
+wB = perf_Wp()
+RGAw()
 distRej()
 perfectControl()
-nyqPlot()
+MIMOnyqPlot()
 U, SV, V = utils.SVD(G(0))
 
 
@@ -490,7 +436,7 @@ print(np.round(SV[0]/SV[2],3))
 
 #for i in range(len(poleValues)):
 #    for j in range(len(poleDirsIn)):
-#        print('%s'%(np.round(poleDirsOut[j,i],4)))
+#        print('%s'%(np.round(poleDirsIn[j,i],4)))
 
 
 
